56c790014bb7dc37c709527d3c82096738f90698
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2015-06-23 13:53:56 -0300

    Emulates animation or transition end event, the end event with longer duration will be used by the emulation

--- packages/metal-anim/test/Anim.js
+++ packages/metal-anim/test/Anim.js
@@ -7,7 +7,7 @@ describe('Anim', function() {
 	beforeEach(function() {
 		this.element = document.createElement('div');
 		this.element.style.transitionDuration = '0.3s';
-		this.element.style.animationDuration = '0.3s';
+		this.element.style.animationDuration = '0.2s';
 		dom.enterDocument(this.element);
 	});
 
@@ -24,24 +24,39 @@ describe('Anim', function() {
 		});
 	});
 
-	it('should emulate animationend after animation-duration', function(done) {
+	it('should emulate animationend after animation-duration interval', function(done) {
 		var start = Date.now();
 		Anim.emulateAnimationEnd(this.element);
 		dom.once(this.element, 'animationend', function() {
-			assert.ok(Date.now() - start >= 300);
+			assert.ok(Date.now() - start >= 200);
 			done();
 		});
 	});
 
-	it('should emulate only the first emitted end event when `emulateEnd` is used', function(done) {
+	it('should emulate the longest duration when `emulateEnd` is used (transitionend)', function(done) {
 		var listener = sinon.stub();
 		dom.once(this.element, 'transitionend', listener);
 		dom.once(this.element, 'animationend', listener);
-		Anim.emulateEnd(this.element, 0);
+		Anim.emulateEnd(this.element);
 		setTimeout(function() {
 			assert.strictEqual(1, listener.callCount);
+			assert.strictEqual('transitionend', listener.args[0][0].customType);
 			done();
-		}, 25);
+		}, 350);
+	});
+
+	it('should emulate the longest duration when `emulateEnd` is used (animationend)', function(done) {
+		this.element.style.transitionDuration = '0.2s';
+		this.element.style.animationDuration = '0.3s';
+		var listener = sinon.stub();
+		dom.once(this.element, 'transitionend', listener);
+		dom.once(this.element, 'animationend', listener);
+		Anim.emulateEnd(this.element);
+		setTimeout(function() {
+			assert.strictEqual(1, listener.callCount);
+			assert.strictEqual('animationend', listener.args[0][0].customType);
+			done();
+		}, 350);
 	});
 
 	it('should not emulate transitionend if already fired', function(done) {
@@ -53,5 +68,15 @@ describe('Anim', function() {
 		setTimeout(function() {
 			done();
 		}, 350);
+	});
+
+	it('should not emulate end event for a specified duration', function(done) {
+		Anim.emulateEnd(this.element, 0);
+		dom.on(this.element, 'animationend', function() {
+			assert.fail('Transition end event must be fired for the same duration');
+		});
+		dom.on(this.element, 'transitionend', function(event) {
+			done();
+		});
 	});
 });
