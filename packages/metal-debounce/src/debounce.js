44805d30a2e7340aa7623089d2b61c327767e579
Maira Bello <maira.araujo@liferay.com>
2015-12-14 13:35:04 -0300

    Implements debounce logic

--- packages/metal-debounce/src/debounce.js
+++ packages/metal-debounce/src/debounce.js
@@ -1,7 +1,20 @@
 'use strict';
 
-class debounce {
-
-}
+/**
+  * Debounces function execution.
+  * @param {Function} fn
+  * @param {Number} delay
+  * @return {Function}
+  */
+ function debounce(fn, delay) {
+   var id;
+   return function() {
+     var args = arguments;
+     clearTimeout(id);
+     id = setTimeout(function() {
+       fn.apply(null, args);
+     }, delay);
+   };
+ }
 
 export default debounce;
