e738fe69ec92b139f90c637ebb10401492b42451
Maira Bello <maira.araujo@liferay.com>
2015-12-14 13:35:04 -0300

    Implements debounce logic

--- packages/metal-debounce/test/debounce.js
+++ packages/metal-debounce/test/debounce.js
@@ -3,7 +3,37 @@
 import debounce from '../src/debounce';
 
 describe('debounce', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
+	it('should only call received function with the last called args after a delay', function(done) {
+		var fn = sinon.stub();
+
+		var debounced = debounce(fn, 200);
+		debounced(1, 2, 3);
+		debounced(4, 5, 6);
+
+		setTimeout(function() {
+			debounced(7, 8, 9);
+			debounced(10, 11, 12);
+			setTimeout(function() {
+				assert.strictEqual(1, fn.callCount);
+				assert.deepEqual([10, 11, 12], fn.args[0]);
+				done();
+			}, 210);
+		}, 100);
+	});
+
+	it('should call original function with its original context', function(done) {
+		var expectedContext = {};
+		var context;
+		var fn = function() {
+			context = this;
+		};
+
+		var debounced = debounce(fn.bind(expectedContext), 200);
+		debounced(1, 2, 3);
+
+		setTimeout(function() {
+			assert.strictEqual(expectedContext, context);
+			done();
+		}, 200);
 	});
 });
