9260ae20019f9e12ca9fc00610717b02bfdad523
Maira Bello <maira.araujo@liferay.com>
2016-01-26 16:17:28 -0300

    Stops using bower.json

--- packages/metal-debounce/README.md
+++ packages/metal-debounce/README.md
@@ -16,7 +16,6 @@ Metal's debounce utility
 
   ```
   npm install
-  bower install
   ```
 
 4. Build the code:
@@ -24,5 +23,3 @@ Metal's debounce utility
   ```
   gulp build
   ```
-
-
--- packages/metal-debounce/src/debounce.js
+++ packages/metal-debounce/src/debounce.js
@@ -6,15 +6,15 @@
   * @param {number} delay
   * @return {!function()}
   */
- function debounce(fn, delay) {
-   var id;
-   return function() {
-     var args = arguments;
-     clearTimeout(id);
-     id = setTimeout(function() {
-       fn.apply(null, args);
-     }, delay);
-   };
- }
+function debounce(fn, delay) {
+	var id;
+	return function() {
+		var args = arguments;
+		clearTimeout(id);
+		id = setTimeout(function() {
+			fn.apply(null, args);
+		}, delay);
+	};
+}
 
 export default debounce;
