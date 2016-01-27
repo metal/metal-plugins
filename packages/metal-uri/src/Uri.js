ed20a4bfd5d63aeabd699a69e3ad11dfb9223f5a
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2016-01-27 18:06:08 -0300

    Adds Uri implementation

--- packages/metal-uri/src/Uri.js
+++ packages/metal-uri/src/Uri.js
@@ -1,6 +1,383 @@
 'use strict';
 
+import core from 'metal/src/core';
+import MultiMap from 'metal-multimap/src/MultiMap';
+
 class Uri {
+
+	/**
+	 * This class contains setters and getters for the parts of the URI.This
+	 * class contains setters and getters for the parts of the URI.
+	 * @param {*=} opt_uri Optional string URI to parse
+	 * @constructor
+	 */
+	constructor(opt_uri = '') {
+		this.url = new URL(this.maybeAddProtocolAndHostname_(opt_uri));
+	}
+
+	/**
+	 * Adds the value of the named query parameters.
+	 * @param {string} key The parameter to set.
+	 * @param {*} value The new value. Will be explicitly casted to String.
+	 * @chainable
+	 */
+	addParameterValue(name, value) {
+		this.ensureQueryInitialized_();
+		if (core.isDef(value)) {
+			value = decodeURIComponent(String(value));
+		}
+		this.query.add(name, value);
+		return this;
+	}
+
+	/**
+	 * Adds the values of the named query parameter.
+	 * @param {string} key The parameter to set.
+	 * @param {*} value The new value.
+	 * @chainable
+	 */
+	addParameterValues(name, values) {
+		values.forEach((value) => this.addParameterValue(name, value));
+		return this;
+	}
+
+	/**
+	 * Ensures query internal map is initialized and synced with initial value
+	 * extracted from URI search part.
+	 * @protected
+	 */
+	ensureQueryInitialized_() {
+		if (this.query) {
+			return;
+		}
+		this.query = new MultiMap();
+		var search = this.url.search;
+		if (search) {
+			search.substring(1).split('&').forEach((param) => {
+				var pieces = param.split('=');
+				this.addParameterValue(pieces[0], pieces[1]);
+			});
+		}
+	}
+
+	/**
+	 * Gets the hash part of uri.
+	 * @return {string}
+	 */
+	getHash() {
+		return this.url.hash;
+	}
+
+	/**
+	 * Gets the host part of uri. E.g. <code>[hostname]:[port]</code>.
+	 * @return {string}
+	 */
+	getHost() {
+		var host = this.getHostname();
+		if (host) {
+			var port = this.getPort();
+			if (port) {
+				host += ':' + port;
+			}
+		}
+		return host;
+	}
+
+	/**
+	 * Gets the hostname part of uri without protocol and port.
+	 * @return {string}
+	 */
+	getHostname() {
+		var hostname = this.url.hostname;
+		if (hostname === Uri.HOSTNAME_PLACEHOLDER) {
+			return '';
+		}
+		return hostname;
+	}
+
+	/**
+	 * Gets the origin part of uri. E.g. <code>http://[hostname]:[port]</code>.
+	 * @return {string}
+	 */
+	getOrigin() {
+		var host = this.getHost();
+		if (host) {
+			return this.getProtocol() + '//' + host;
+		}
+		return '';
+	}
+
+	/**
+	 * Returns the first value for a given parameter or undefined if the given
+	 * parameter name does not appear in the query string.
+	 * @param {string} paramName Unescaped parameter name.
+	 * @return {string|undefined} The first value for a given parameter or
+	 *   undefined if the given parameter name does not appear in the query
+	 *   string.
+	 */
+	getParameterValue(name) {
+		this.ensureQueryInitialized_();
+		return this.query.get(name);
+	}
+
+	/**
+	 * Returns the value<b>s</b> for a given parameter as a list of decoded
+	 * query parameter values.
+	 * @param {string} name The parameter to get values for.
+	 * @return {!Array<?>} The values for a given parameter as a list of decoded
+	 *   query parameter values.
+	 */
+	getParameterValues(name) {
+		this.ensureQueryInitialized_();
+		return this.query.getAll(name);
+	}
+
+	/**
+	 * Returns the name<b>s</b> of the parameters.
+	 * @return {!Array<string>} The names for the parameters as a list of
+	 *   strings.
+	 */
+	getParameterNames() {
+		this.ensureQueryInitialized_();
+		return this.query.names();
+	}
+
+	/**
+	 * Gets the password part of uri.
+	 * @return {string}
+	 */
+	getPassword() {
+		return this.url.password;
+	}
+
+	/**
+	 * Gets the pathname part of uri.
+	 * @return {string}
+	 */
+	getPathname() {
+		return this.url.pathname;
+	}
+
+	/**
+	 * Gets the port number part of uri as string.
+	 * @return {string}
+	 */
+	getPort() {
+		return this.url.port;
+	}
+
+	/**
+	 * Gets the protocol part of uri. E.g. <code>http:</code>.
+	 * @return {string}
+	 */
+	getProtocol() {
+		return this.url.protocol;
+	}
+
+	/**
+	 * Gets the search part of uri. Search value is retrieved from query
+	 * parameters.
+	 * @return {string}
+	 */
+	getSearch() {
+		var search = '';
+		var querystring = '';
+		this.getParameterNames().forEach((name) => {
+			this.getParameterValues(name).forEach((value) => {
+				querystring += name;
+				if (core.isDef(value)) {
+					querystring += '=' + encodeURIComponent(value);
+				}
+				querystring += '&';
+			});
+		});
+		querystring = querystring.slice(0, -1);
+		if (querystring) {
+			search += '?' + querystring;
+		}
+		return search;
+	}
+
+	/**
+	 * Gets the username part of uri.
+	 * @return {string}
+	 */
+	getUsername() {
+		return this.url.username;
+	}
+
+	/**
+	 * Maybe adds protocol and a hostname placeholder on a parial URI if needed.
+	 * Relevent for compatibility with <code>URL</code> native object.
+	 * @param {string=} opt_uri
+	 * @return {string} URI with protocol and hostname placeholder.
+	 */
+	maybeAddProtocolAndHostname_(opt_uri) {
+		var url = opt_uri;
+		if (opt_uri.indexOf('://') === -1) {
+			url = Uri.DEFAULT_PROTOCOL + '//';
+
+			switch (opt_uri.charAt(0)) {
+				case '.':
+				case '?':
+				case '#':
+					url += Uri.HOSTNAME_PLACEHOLDER;
+					url += '/';
+					url += opt_uri;
+					break;
+				case '':
+				case '/':
+					url += Uri.HOSTNAME_PLACEHOLDER;
+					url += opt_uri;
+					break;
+				default:
+					url += opt_uri;
+			}
+		}
+		return url;
+	}
+
+	/**
+	 * Removes the named query parameter.
+	 * @param {string} name The parameter to remove.
+	 * @chainable
+	 */
+	removeParameter(name) {
+		this.ensureQueryInitialized_();
+		this.query.remove(name);
+		return this;
+	}
+
+	/**
+	 * Sets the hash.
+	 * @param {string} hash
+	 * @chainable
+	 */
+	setHash(hash) {
+		this.url.hash = hash;
+		return this;
+	}
+
+	/**
+	 * Sets the hostname.
+	 * @param {string} hostname
+	 * @chainable
+	 */
+	setHostname(hostname) {
+		this.url.hostname = hostname;
+		return this;
+	}
+
+	/**
+	 * Sets the value of the named query parameters, clearing previous values
+	 * for that key.
+	 * @param {string} key The parameter to set.
+	 * @param {*} value The new value.
+	 * @chainable
+	 */
+	setParameterValue(name, value) {
+		this.removeParameter(name);
+		this.addParameterValue(name, value);
+		return this;
+	}
+
+	/**
+	 * Sets the values of the named query parameters, clearing previous values
+	 * for that key.
+	 * @param {string} key The parameter to set.
+	 * @param {*} value The new value.
+	 * @chainable
+	 */
+	setParameterValues(name, values) {
+		this.removeParameter(name);
+		values.forEach((value) => this.addParameterValue(name, value));
+		return this;
+	}
+
+	/**
+	 * Sets the password.
+	 * @param {string} password
+	 * @chainable
+	 */
+	setPassword(password) {
+		this.url.password = password;
+		return this;
+	}
+
+	/**
+	 * Sets the pathname.
+	 * @param {string} pathname
+	 * @chainable
+	 */
+	setPathname(pathname) {
+		this.url.pathname = pathname;
+		return this;
+	}
+
+	/**
+	 * Sets the port number.
+	 * @param {*} port Port number.
+	 * @chainable
+	 */
+	setPort(port) {
+		this.url.port = port;
+		return this;
+	}
+
+	/**
+	 * Sets the protocol. If missing <code>http:</code> is used as default.
+	 * @param {string} protocol
+	 * @chainable
+	 */
+	setProtocol(protocol) {
+		this.url.protocol = protocol;
+		return this;
+	}
+
+	/**
+	 * Sets the username.
+	 * @param {string} username
+	 * @chainable
+	 */
+	setUsername(username) {
+		this.url.username = username;
+		return this;
+	}
+
+	/**
+	 * @return {string} The string form of the url.
+	 * @override
+	 */
+	toString() {
+		var href = '';
+		var host = this.getHost();
+		if (host) {
+			href += this.getProtocol() + '//';
+			var username = this.getUsername();
+			var password = this.getPassword();
+			if (username && password) {
+				href += username + ':' + password + '@';
+			}
+		}
+		href += host + this.getPathname() + this.getSearch() + this.getHash();
+		return href;
+	}
+
 }
+
+/**
+ * Default protocol value.
+ * @type {String}
+ * @default http:
+ * @static
+ */
+Uri.DEFAULT_PROTOCOL = 'http:';
+
+/**
+ * Hostname placeholder. Relevant to internal usage only.
+ * @type {String}
+ * @static
+ */
+Uri.HOSTNAME_PLACEHOLDER = 'hostname' + Date.now();
 
 export default Uri;
--- packages/metal-uri/test/Uri.js
+++ packages/metal-uri/test/Uri.js
@@ -3,7 +3,208 @@
 import Uri from '../src/Uri';
 
 describe('Uri', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
+	it('should support empty uri', function() {
+		var uri = new Uri();
+		assert.strictEqual('/', uri.getPathname());
+		assert.strictEqual('/', uri.toString());
 	});
+
+	it('should get uri hostname', function() {
+		var uri = new Uri('http://hostname:8080');
+		assert.strictEqual('hostname', uri.getHostname());
+	});
+
+	it('should get uri host', function() {
+		var uri = new Uri('http://hostname:8080');
+		assert.strictEqual('hostname:8080', uri.getHost());
+	});
+
+	it('should get uri origin', function() {
+		var uri = new Uri('http://hostname:8080/ignore');
+		assert.strictEqual('http://hostname:8080', uri.getOrigin());
+	});
+
+	it('should hostname be empty for empty uri', function() {
+		var uri = new Uri();
+		assert.strictEqual('', uri.getHostname());
+	});
+
+	it('should host be empty for empty uri', function() {
+		var uri = new Uri();
+		assert.strictEqual('', uri.getHost());
+	});
+
+	it('should origin be empty for empty uri', function() {
+		var uri = new Uri();
+		assert.strictEqual('', uri.getOrigin());
+	});
+
+	it('should support set port on uri', function() {
+		var uri = new Uri('hostname:8080');
+		assert.strictEqual('8080', uri.getPort());
+		uri.setPort(81);
+		assert.strictEqual('81', uri.getPort());
+	});
+
+	it('should support set protocol on uri', function() {
+		var uri = new Uri('http://hostname');
+		assert.strictEqual('http:', uri.getProtocol());
+		uri.setProtocol('https');
+		assert.strictEqual('https:', uri.getProtocol());
+		uri.setProtocol('https:');
+		assert.strictEqual('https:', uri.getProtocol());
+	});
+
+	it('should support only hostname', function() {
+		var uri = new Uri('hostname');
+		assert.strictEqual('hostname', uri.getHostname());
+		assert.strictEqual('http://hostname/', uri.toString());
+	});
+
+	it('should support only hostname from setter', function() {
+		var uri = new Uri();
+		uri.setHostname('hostname');
+		assert.strictEqual('hostname', uri.getHostname());
+		assert.strictEqual('http://hostname/', uri.toString());
+	});
+
+	it('should support only pathname', function() {
+		var uri = new Uri('/pathname');
+		assert.strictEqual('/pathname', uri.getPathname());
+		assert.strictEqual('/pathname', uri.toString());
+	});
+
+	it('should support only pathname from setter', function() {
+		var uri = new Uri();
+		uri.setPathname('/pathname');
+		assert.strictEqual('/pathname', uri.getPathname());
+		assert.strictEqual('/pathname', uri.toString());
+	});
+
+	it('should support only hash', function() {
+		var uri = new Uri('#hash');
+		assert.strictEqual('#hash', uri.getHash());
+		assert.strictEqual('/#hash', uri.toString());
+	});
+
+	it('should support only hash from setter', function() {
+		var uri = new Uri();
+		uri.setHash('#hash');
+		assert.strictEqual('#hash', uri.getHash());
+		assert.strictEqual('/#hash', uri.toString());
+	});
+
+	it('should support only search', function() {
+		assert.strictEqual('/?&=&=&', new Uri('?&=&=&').toString());
+		assert.strictEqual('/?a=1', new Uri('?a=1').toString());
+		assert.strictEqual('/?a=1', new Uri('?a=1').toString());
+		assert.strictEqual('/?foo', new Uri('?foo').toString());
+		assert.strictEqual('/?foo=', new Uri('?foo=').toString());
+		assert.strictEqual('/?foo=bar', new Uri('?foo=bar').toString());
+		assert.strictEqual('?&=&=&', new Uri('?&=&=&').getSearch());
+		assert.strictEqual('?a=1', new Uri('?a=1').getSearch());
+		assert.strictEqual('?a=1', new Uri('?a=1').getSearch());
+		assert.strictEqual('?foo', new Uri('?foo').getSearch());
+		assert.strictEqual('?foo=', new Uri('?foo=').getSearch());
+		assert.strictEqual('?foo=bar', new Uri('?foo=bar').getSearch());
+	});
+
+	it('should support pathname with search', function() {
+		var uri = new Uri('/pathname?a=1');
+		assert.strictEqual('?a=1', uri.getSearch());
+		assert.strictEqual('/pathname', uri.getPathname());
+		assert.strictEqual('/pathname?a=1', uri.toString());
+	});
+
+	it('should support pathname with search and hash', function() {
+		var uri = new Uri('/pathname?a=1#hash');
+		assert.strictEqual('#hash', uri.getHash());
+		assert.strictEqual('?a=1', uri.getSearch());
+		assert.strictEqual('/pathname', uri.getPathname());
+		assert.strictEqual('/pathname?a=1#hash', uri.toString());
+	});
+
+	it('should support username and password', function() {
+		var uri = new Uri('http://user:pass@hostname');
+		assert.strictEqual('user', uri.getUsername());
+		assert.strictEqual('pass', uri.getPassword());
+		assert.strictEqual('http://user:pass@hostname/', uri.toString());
+	});
+
+	it('should support username and password from setter', function() {
+		var uri = new Uri('http://hostname');
+		uri.setUsername('user');
+		uri.setPassword('pass');
+		assert.strictEqual('user', uri.getUsername());
+		assert.strictEqual('pass', uri.getPassword());
+		assert.strictEqual('http://user:pass@hostname/', uri.toString());
+	});
+
+	it('should initialize parameters from search part of uri', function() {
+		var uri = new Uri('http://hostname?a=1&b=2');
+		assert.strictEqual('1', uri.getParameterValue('a'));
+		assert.strictEqual('2', uri.getParameterValue('b'));
+	});
+
+	it('should set parameter value in uri', function() {
+		var uri = new Uri();
+		uri.setParameterValue('a', '1');
+		uri.setParameterValue('a', '2');
+		assert.strictEqual('2', uri.getParameterValue('a'));
+		assert.deepEqual(['2'], uri.getParameterValues('a'));
+	});
+
+	it('should set parameter values in uri', function() {
+		var uri = new Uri();
+		uri.setParameterValues('a', ['1', '2']);
+		assert.strictEqual('1', uri.getParameterValue('a'));
+		assert.deepEqual(['1', '2'], uri.getParameterValues('a'));
+	});
+
+	it('should add parameter value in uri', function() {
+		var uri = new Uri();
+		uri.addParameterValue('a', '1');
+		uri.addParameterValue('a', '2');
+		assert.strictEqual('1', uri.getParameterValue('a'));
+		assert.deepEqual(['1', '2'], uri.getParameterValues('a'));
+	});
+
+	it('should add parameter values in uri', function() {
+		var uri = new Uri();
+		uri.addParameterValues('a', ['1', '2']);
+		assert.strictEqual('1', uri.getParameterValue('a'));
+		assert.deepEqual(['1', '2'], uri.getParameterValues('a'));
+	});
+
+	it('should remove parameter in uri', function() {
+		var uri = new Uri();
+		uri.setParameterValue('a', '1');
+		uri.removeParameter('a');
+		assert.strictEqual(undefined, uri.getParameterValue('a'));
+	});
+
+	it('should add parameter value in uri be case insensitive', function() {
+		var uri = new Uri();
+		uri.addParameterValue('a', '1');
+		uri.addParameterValue('A', '2');
+		uri.addParameterValue('a', '3');
+		assert.strictEqual('1', uri.getParameterValue('a'));
+		assert.strictEqual('1', uri.getParameterValue('A'));
+		assert.deepEqual(['1', '2', '3'], uri.getParameterValues('a'));
+		assert.deepEqual(['1', '2', '3'], uri.getParameterValues('A'));
+		assert.strictEqual('/?a=1&a=2&a=3', uri.toString());
+	});
+
+	it('should encode parameter value in uri', function() {
+		var uri = new Uri();
+		uri.setParameterValue('a', 'one space');
+		assert.strictEqual('one space', uri.getParameterValue('a'));
+		assert.strictEqual('/?a=one%20space', uri.toString());
+	});
+
+	it('should remove dot segments from pathname', function() {
+		assert.strictEqual('/a/', new Uri('./a/').getPathname());
+		assert.strictEqual('/a/', new Uri('./a/b/c/../..').getPathname());
+	});
+
 });
