70812281209dccb73e01f6cd47d9fe2da3e7f994
Maira Bello <maira.araujo@liferay.com>
2016-02-02 17:07:33 -0300

    Fixes URL calls for all browsers
    Browsers that correctly implement URL will have it as a function and will have its "length" property equal to 1. This diff changes the feature check to use that logic.

--- packages/metal-uri/src/parse.js
+++ packages/metal-uri/src/parse.js
@@ -3,13 +3,7 @@
 import { core } from 'metal';
 import parseFromAnchor from './parseFromAnchor';
 
-var hasUrlFn_;
-function hasUrlFn() {
-	if (!core.isDef(hasUrlFn_)) {
-		hasUrlFn_ = core.isFunction(URL) && new URL('http://test').hostname;
-	}
-	return hasUrlFn_;
-}
+var hasUrlFn_ = core.isFunction(URL) && URL.length;
 
 /**
  * Parses the given uri string into an object. The URL function will be used
@@ -17,7 +11,7 @@ function hasUrlFn() {
  * @param {*=} opt_uri Optional string URI to parse
  */
 function parse(opt_uri) {
-	if (hasUrlFn()) {
+	if (hasUrlFn_) {
 		return new URL(opt_uri);
 	} else {
 		return parseFromAnchor(opt_uri);
