09026157cfe0811fd5af702d78ee75d29a1fec53
Maira Bello <maira.araujo@liferay.com>
2016-02-02 17:12:44 -0300

    Always do the feature check inside "parse"
    That's to avoid errors on node environment.

--- packages/metal-uri/src/parse.js
+++ packages/metal-uri/src/parse.js
@@ -3,15 +3,13 @@
 import { core } from 'metal';
 import parseFromAnchor from './parseFromAnchor';
 
-var hasUrlFn_ = core.isFunction(URL) && URL.length;
-
 /**
  * Parses the given uri string into an object. The URL function will be used
  * when present, otherwise we'll fall back to the anchor node element.
  * @param {*=} opt_uri Optional string URI to parse
  */
 function parse(opt_uri) {
-	if (hasUrlFn_) {
+	if (core.isFunction(URL) && URL.length) {
 		return new URL(opt_uri);
 	} else {
 		return parseFromAnchor(opt_uri);
