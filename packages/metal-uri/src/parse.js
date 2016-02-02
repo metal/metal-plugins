46cb8c185455ce3d4b667713b11ee5e437932f39
Maira Bello <maira.araujo@liferay.com>
2016-02-02 16:56:43 -0300

    Fixes Uri when used by Chrome Mobile 30.0.0
    Chrome Mobile 30.0.0 has the URL function, but it doesn't work at all. This enhances the feature check for the URL function to make sure it works properly.

--- packages/metal-uri/src/parse.js
+++ packages/metal-uri/src/parse.js
@@ -3,13 +3,21 @@
 import { core } from 'metal';
 import parseFromAnchor from './parseFromAnchor';
 
+var hasUrlFn_;
+function hasUrlFn() {
+	if (!core.isDef(hasUrlFn_)) {
+		hasUrlFn_ = core.isFunction(URL) && new URL('http://test').hostname;
+	}
+	return hasUrlFn_;
+}
+
 /**
  * Parses the given uri string into an object. The URL function will be used
  * when present, otherwise we'll fall back to the anchor node element.
  * @param {*=} opt_uri Optional string URI to parse
  */
 function parse(opt_uri) {
-	if (core.isFunction(URL)) {
+	if (hasUrlFn()) {
 		return new URL(opt_uri);
 	} else {
 		return parseFromAnchor(opt_uri);
