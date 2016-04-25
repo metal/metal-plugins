a2fa20c5986464e05ae29270473bf11c9c0d8a64
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2016-04-25 15:42:07 -0700

    Fist commit

---
+++ packages/metal-structs/gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'structs.css',
+	bundleFileName: 'structs.js',
+	moduleName: 'metal-structs',
+	noSoy: true
+});
