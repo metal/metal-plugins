e219c74cb20c490ee604bcb56ff3195a581f5e13
Maira Bello <maira.araujo@liferay.com>
2016-04-28 11:54:29 -0300

    Implements the Provider component

--- packages/metal-redux/src/Provider.js
+++ packages/metal-redux/src/Provider.js
@@ -1,8 +1,38 @@
 'use strict';
 
-import core from 'metal';
+import Component from 'metal-component';
+import IncrementalDomRenderer from 'metal-incremental-dom';
 
-class Provider {
+/**
+ * Provides a redux store instance to all descendant components.
+ */
+class Provider extends Component {
+	/**
+	 * Defines the store as a child context, meaning that it will be passed down
+	 * to all descendant components.
+	 * @override
+	 */
+	getChildContext() {
+		return {
+			store: this.config.store
+		};
+	}
 }
+
+/**
+ * The renderer used by `Provider`.
+ */
+class ProviderRenderer extends IncrementalDomRenderer {
+	/**
+	 * Renders a wrapper element for the provider (since it's required by
+	 * Metal.js), and its children inside it.
+	 * @override
+	 */
+	renderIncDom() {
+		this.component_.config.children();
+	}
+}
+
+Provider.RENDERER = ProviderRenderer;
 
 export default Provider;
--- packages/metal-redux/test/Provider.js
+++ packages/metal-redux/test/Provider.js
@@ -1,9 +1,75 @@
 'use strict';
 
+import Component from 'metal-component';
+import IncrementalDomRenderer from 'metal-incremental-dom';
 import Provider from '../src/Provider';
 
 describe('Provider', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
+	var ChildComponent;
+	var component;
+
+	beforeEach(function() {
+		ChildComponent = createTestComponentClass();
+		ChildComponent.RENDERER.prototype.renderIncDom = function() {
+			IncrementalDOM.elementOpen('child');
+			IncrementalDOM.text('Child');
+			IncrementalDOM.elementClose('child');
+		};
 	});
+
+	afterEach(function() {
+		if (component) {
+			component.dispose();
+		}
+	});
+
+	it('should render the children received', function() {
+		var TestComponent = createTestComponentClass();
+		TestComponent.RENDERER.prototype.renderIncDom = function() {
+			IncrementalDOM.elementOpen(Provider, 'provider');
+			IncrementalDOM.elementVoid(ChildComponent, 'child');
+			IncrementalDOM.elementClose(Provider);
+		};
+
+		component = new TestComponent();
+		var provider = component.components.provider;
+		var child = component.components.child;
+		assert.ok(provider);
+		assert.ok(child);
+		assert.strictEqual(provider.element, component.element);
+		assert.strictEqual(child.element, provider.element);
+		assert.strictEqual(1, component.element.childNodes.length);
+		assert.strictEqual('CHILD', component.element.tagName);
+		assert.strictEqual('Child', component.element.textContent);
+	});
+
+	it('should pass store object down to children as context', function() {
+		var store = {};
+		var TestComponent = createTestComponentClass();
+		TestComponent.RENDERER.prototype.renderIncDom = function() {
+			IncrementalDOM.elementOpen(Provider, 'provider', ['store', store]);
+			IncrementalDOM.elementVoid(ChildComponent, 'child');
+			IncrementalDOM.elementClose(Provider);
+		};
+
+		component = new TestComponent();
+		var provider = component.components.provider;
+		var child = component.components.child;
+		assert.ok(!component.context.store);
+		assert.ok(!provider.context.store);
+		assert.strictEqual(store, child.context.store);
+	});
+
+	function createTestComponentClass(opt_renderer) {
+		class TestComponent extends Component {
+		}
+		TestComponent.RENDERER = opt_renderer || createIncrementalDomRenderer();
+		return TestComponent;
+	}
+
+	function createIncrementalDomRenderer() {
+		class TestRenderer extends IncrementalDomRenderer {
+		}
+		return TestRenderer;
+	}
 });
