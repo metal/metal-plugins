f37fb9bb66bca1a0eb66c512dd413d6fa36049af
Maira Bello <maira.araujo@liferay.com>
2016-05-17 15:38:42 -0300

    Stops using nodejs url code when running on browser

--- packages/metal-uri/node.js
+++ packages/metal-uri/node.js
@@ -1,13 +1,18 @@
 'use strict';
 
-var path = require('path');
-var url = require('url');
 var Uri = require('./lib/Uri').default;
 
-Uri.setParseFn(function(urlStr) {
-	var parsed = url.parse(urlStr);
-	parsed.pathname = path.normalize(parsed.pathname);
-	return parsed;
-});
+if (typeof document === 'undefined') {
+	// If there's no document, then this should be running in NodeJS, and so
+	// we should use the "url" node module as the parse function.
+
+	var path = require('path');
+	var url = require('url');
+	Uri.setParseFn(function(urlStr) {
+		var parsed = url.parse(urlStr);
+		parsed.pathname = path.normalize(parsed.pathname);
+		return parsed;
+	});
+}
 
 module.exports = Uri;
