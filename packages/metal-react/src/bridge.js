4eb37aded10912546b6b0671309f44380e0d414a
Maira Bello <maira.araujo@liferay.com>
2016-05-30 15:38:32 -0300

    Implements bridge

--- packages/metal-react/src/bridge.js
+++ packages/metal-react/src/bridge.js
@@ -1,10 +1,45 @@
 'use strict';
 
+import { object } from 'metal';
 import Component from 'metal-component';
+import IncrementalDomRenderer from 'metal-incremental-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/**
+ * Creates a bridge component between Metal.js and React. This enables using
+ * React components in Metal.js's environment.
+ * @param {!function()} ReactComponent A React component's constructor
+ * @return {!functon()} A metal component wrapping the given React one.
+ */
+export default function(ReactComponent) {
+	class BridgeComponent extends Component {
+		/**
+		 * Returns the current instance for the react component.
+		 */
+		getInstance() {
+			return this.instance_;
+		}
 
-export default function() {
+		/**
+		 * Renders this component, by letting react do its job inside a given
+		 * container element. Passes all received config properties to the react
+		 * component as props.
+		 */
+		render() {
+			IncrementalDOM.elementOpen('div', null, [], 'class', 'metal-react-container');
+			var element = IncrementalDOM.currentElement();
+			IncrementalDOM.skip();
+			IncrementalDOM.elementClose('div');
 
+			var data = object.mixin({}, this.config);
+			delete data.children;
+			this.instance_ = ReactDOM.render(
+			  React.createElement(ReactComponent, data),
+			  element
+			);
+		}
+	}
+	BridgeComponent.RENDERER = IncrementalDomRenderer;
+	return BridgeComponent;
 }
--- packages/metal-react/test/bridge.js
+++ packages/metal-react/test/bridge.js
@@ -1,9 +1,46 @@
 'use strict';
 
 import bridge from '../src/bridge';
+import dom from 'metal-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 describe('bridge', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
+	var component;
+	var ReactComponent = () => {};
+
+	beforeEach(function() {
+		sinon.spy(React, 'createElement');
+		sinon.stub(ReactDOM, 'render');
+	});
+
+	afterEach(function() {
+		if (component) {
+			component.dispose();
+		}
+		React.createElement.restore();
+		ReactDOM.render.restore();
+	});
+
+	it('should render react component inside container', function() {
+		var BridgeComponent = bridge(ReactComponent);
+		component = new BridgeComponent();
+
+		assert.ok(dom.hasClass(component.element, 'metal-react-container'));
+		assert.strictEqual(1, React.createElement.callCount);
+		assert.strictEqual(ReactComponent, React.createElement.args[0][0]);
+		assert.strictEqual(1, ReactDOM.render.callCount);
+		assert.strictEqual(component.element, ReactDOM.render.args[0][1]);
+	});
+
+	it('should pass config as props', function() {
+		var BridgeComponent = bridge(ReactComponent);
+		var config = {
+			foo: 'fooValue',
+			bar: 'barValue'
+		};
+		component = new BridgeComponent(config);
+
+		assert.deepEqual(config, React.createElement.args[0][1]);
 	});
 });
