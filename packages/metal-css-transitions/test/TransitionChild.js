1241a72d60fb5272e08c914ed96c33ce7662382c
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2016-06-07 12:26:56 -0700

    Initial Commit

---
+++ packages/metal-css-transitions/test/TransitionChild.js
@@ -0,0 +1,145 @@
+'use strict';
+import JSXComponent from 'metal-jsx';
+
+import TransitionChild, { DELAY_TIME } from '../src/TransitionChild';
+
+describe('TransitionChild', function() {
+	let component;
+
+	afterEach(
+		() => {
+			if (component) {
+				component.dispose();
+			}
+		}
+	);
+
+	it(
+		'renders',
+		() => {
+			const component = new TransitionChild();
+
+			assert(component);
+		}
+	);
+
+	it('should delay before adding a class', function(done) {
+		const component = new TransitionChild();
+		const node = document.createElement('div');
+
+		assert.strictEqual('', node.className);
+		component.delayActive('test', node);
+		assert.strictEqual('', node.className);
+
+		setTimeout(
+			() => {
+				assert.strictEqual('test', node.className);
+
+				done();
+			},
+			DELAY_TIME
+		);
+	});
+
+	it('should add and remove classnames', function(done) {
+		const DELAY = 100;
+		const TRANS_NAME = 'test';
+		const TRANS_TYPE = 'appear';
+
+		class App extends JSXComponent {
+			render() {
+				return (
+					<TransitionChild transitionName={TRANS_NAME} ref="transitionChild">
+						<div></div>
+					</TransitionChild>
+					);
+			}
+		}
+
+		const app = new App();
+		const appElem = app.element;
+
+		const transitionChild = app.components.transitionChild;
+
+		transitionChild.transition(TRANS_TYPE, null, DELAY);
+
+		assert.strictEqual(`${TRANS_NAME}-${TRANS_TYPE}`, appElem.className);
+
+		setTimeout(
+			() => {
+				assert.strictEqual(`${TRANS_NAME}-${TRANS_TYPE} ${TRANS_NAME}-${TRANS_TYPE}-active`, appElem.className);
+
+				setTimeout(
+					() => {
+						assert.strictEqual('', appElem.className);
+
+						done();
+					},
+					DELAY
+				);
+			},
+			DELAY_TIME
+		);
+	});
+
+	it('should execute callbackFn', function(done) {
+		const DELAY = 100;
+		const stubFn = sinon.stub();
+
+		class App extends JSXComponent {
+			render() {
+				return (
+					<TransitionChild transitionName="test" ref="transitionChild">
+						<div></div>
+					</TransitionChild>
+					);
+			}
+		}
+
+		const app = new App();
+		const transitionChild = app.components.transitionChild;
+
+		transitionChild.transition(null, stubFn, DELAY);
+
+		assert.isFalse(stubFn.called);
+
+		setTimeout(
+			() => {
+				assert.isTrue(stubFn.called);
+
+				done();
+			},
+			DELAY
+		);
+	});
+
+	it('should call transition with appear', function() {
+		const component = new TransitionChild();
+
+		const transitionFn = sinon.stub(component, 'transition');
+
+		assert.isFalse(transitionFn.called);
+		component.appear();
+		assert.isTrue(transitionFn.calledWith('appear'));
+	});
+
+	it('should call transition with enter', function() {
+		const component = new TransitionChild();
+
+		const transitionFn = sinon.stub(component, 'transition');
+
+		assert.isFalse(transitionFn.called);
+		component.enter();
+		assert.isTrue(transitionFn.calledWith('enter'));
+	});
+
+	it('should call transition with leave', function() {
+		const component = new TransitionChild();
+
+		const transitionFn = sinon.stub(component, 'transition');
+
+		assert.isFalse(transitionFn.called);
+		component.leave();
+		assert.isTrue(transitionFn.calledWith('leave'));
+	});
+});
