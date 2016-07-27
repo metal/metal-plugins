bf3a2a64343e34cb37adc6124cc27be645bc92ef
Maira Bello <maira.araujo@liferay.com>
2016-07-27 17:02:38 -0300

    Adds initial structure

---
+++ packages/metal-path-parser/test/PathParser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import PathParser from '../src/PathParser';
+
+describe('PathParser', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
