8645aa25f4c61f7067438d518c0cc3f4baf9a4a2
Maira Bello <maira.araujo@liferay.com>
2016-07-27 17:02:56 -0300

    Adds function for parsing paths into tokens

--- packages/metal-path-parser/test/PathParser.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import PathParser from '../src/PathParser';
-
-describe('PathParser', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
-	});
-});
