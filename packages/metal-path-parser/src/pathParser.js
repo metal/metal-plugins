e665cb8941a399dd3fa116e5468eba2d5c2a268f
Maira Bello <maira.araujo@liferay.com>
2016-07-27 17:02:56 -0300

    Adds function for parsing paths into tokens

---
+++ packages/metal-path-parser/src/pathParser.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var regex = /\/([^\/:(]*)(?:\:([A-Za-z0-9_]+))?(?:\(([^)]*)\))?([^\/]*)/g;
+
+/**
+ * Guarantes that the path will start with a "/".
+ */
+function normalizePath(path) {
+	return path[0] === '/' ? path : '/' + path;
+}
+
+export function parse(path) {
+	path = normalizePath(path);
+	let unnamedCount = 0;
+	const tokens = [];
+	let currPath = '';
+
+	let matches = regex.exec(path);
+	while (matches) {
+		currPath += '/' + matches[1];
+		if (matches[2] || matches[3]) {
+			if (currPath) {
+				tokens.push(currPath);
+				currPath = '';
+			}
+			tokens.push({
+				name: matches[2] ? matches[2] : '' + unnamedCount++,
+				pattern: matches[3]
+			});
+		}
+		currPath += matches[4];
+		matches = regex.exec(path);
+	}
+
+	if (currPath) {
+		tokens.push(currPath);
+	}
+	return tokens;
+}
