01fdd379aa4e87c643f690444e3bfe858938e1a9
Maira Bello <maira.araujo@liferay.com>
2016-07-28 11:48:27 -0300

    Adds function that turns a path into a regex string to fetch param data

--- packages/metal-path-parser/test/pathParser.js
+++ packages/metal-path-parser/test/pathParser.js
@@ -1,28 +1,28 @@
 'use strict';
 
-import { parse } from '../src/pathParser';
+import { parse, toRegex } from '../src/pathParser';
 
 describe('pathParser', function() {
 	describe('parse', function() {
 		it('should parse simple path with no params', function() {
-			var parsed = parse('/my/path');
+			const parsed = parse('/my/path');
 			assert.deepEqual(['/my/path'], parsed);
 		});
 
 		it('should parse path with params', function() {
-			var parsed = parse('/my/path/prefix-:foo/and/:bar-suffix');
+			const parsed = parse('/my/path/prefix-:foo/and/:bar-suffix');
 			assert.strictEqual(5, parsed.length);
 			assert.strictEqual('/my/path/prefix-', parsed[0]);
 			assert.strictEqual('foo', parsed[1].name);
-			assert.ok(!parsed[1].pattern);
+			assert.strictEqual('[^\\/]+', parsed[1].pattern);
 			assert.strictEqual('/and/', parsed[2]);
 			assert.strictEqual('bar', parsed[3].name);
-			assert.ok(!parsed[3].pattern);
+			assert.strictEqual('[^\\/]+', parsed[3].pattern);
 			assert.strictEqual('-suffix', parsed[4]);
 		});
 
 		it('should parse param with pattern', function() {
-			var parsed = parse('/my/path/:foo(\\d+)');
+			const parsed = parse('/my/path/:foo(\\d+)');
 			assert.strictEqual(2, parsed.length);
 			assert.strictEqual('/my/path/', parsed[0]);
 			assert.strictEqual('foo', parsed[1].name);
@@ -30,15 +30,15 @@ describe('pathParser', function() {
 		});
 
 		it('should parse param with empty pattern', function() {
-			var parsed = parse('/my/path/:foo()');
+			const parsed = parse('/my/path/:foo()');
 			assert.strictEqual(2, parsed.length);
 			assert.strictEqual('/my/path/', parsed[0]);
 			assert.strictEqual('foo', parsed[1].name);
-			assert.strictEqual('', parsed[1].pattern);
+			assert.strictEqual('[^\\/]+', parsed[1].pattern);
 		});
 
 		it('should parse unnamed param patterns', function() {
-			var parsed = parse('/my/path/(\\w+)/prefix-(\\d+)');
+			const parsed = parse('/my/path/(\\w+)/prefix-(\\d+)');
 			assert.strictEqual(4, parsed.length);
 			assert.strictEqual('/my/path/', parsed[0]);
 			assert.strictEqual('0', parsed[1].name);
@@ -46,6 +46,32 @@ describe('pathParser', function() {
 			assert.strictEqual('/prefix-', parsed[2]);
 			assert.strictEqual('1', parsed[3].name);
 			assert.strictEqual('\\d+', parsed[3].pattern);
+		});
+	});
+
+	describe('toRegex', function() {
+		it('should return the original path if it has no params', function() {
+			assert.strictEqual('/\\/my\\/path/', toRegex('/my/path').toString());
+		});
+
+		it('should return regex that will match params', function() {
+			const regex = toRegex('/my/path/prefix-:foo/and/:bar-suffix');
+			assert.ok(!'/not/my/path'.match(regex));
+
+			const match = '/my/path/prefix-10/and/20-suffix'.match(regex);
+			assert.ok(match);
+			assert.strictEqual('10', match[1]);
+			assert.strictEqual('20', match[2]);
+		});
+
+		it('should return regex that will match params according to their own regex', function() {
+			const regex = toRegex('/my/path/:foo(\\d+)');
+			assert.ok(!'/not/my/path'.match(regex));
+			assert.ok(!'/my/path/nan'.match(regex));
+
+			const match = '/my/path/42'.match(regex);
+			assert.ok(match);
+			assert.strictEqual('42', match[1]);
 		});
 	});
 });
