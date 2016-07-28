bf80af5ba8b3e2401964e9eb557fa58306f4ce26
Maira Bello <maira.araujo@liferay.com>
2016-07-28 17:44:20 -0300

    Adds docs

--- packages/metal-path-parser/src/pathParser.js
+++ packages/metal-path-parser/src/pathParser.js
@@ -4,6 +4,11 @@ import core from 'metal';
 
 const REGEX = /([\/])?(?:(?:\:(\w+)(?:\(((?:\\.|[^\\()])*)\))?|\(((?:\\.|[^\\()])+)\))([+*?])?)/g;
 
+/**
+ * Converts the given array of regex matches to a more readable object format.
+ * @param {!Array<string>} matches
+ * @return {!Object}
+ */
 function convertMatchesToObj(matches) {
 	return {
 		match: matches[0],
@@ -15,6 +20,11 @@ function convertMatchesToObj(matches) {
 	};
 }
 
+/**
+ * Converts the given tokens parsed from a route format string to a regex.
+ * @param {!Array<string|!Object>} tokens
+ * @return {!RegExp}
+ */
 function convertTokensToRegex(tokens) {
 	let regex = '';
 	for (let i = 0; i < tokens.length; i++) {
@@ -56,6 +66,11 @@ function normalizePath(route) {
 	return route[0] === '/' ? route : '/' + route;
 }
 
+/**
+ * Parses the given route format string into tokens representing its contents.
+ * @return {!Array<string|!Object>} An array of tokens that can be either plain
+ *     strings (part of the route) or objects containing informations on params.
+ */
 export function parse(route) {
 	route = normalizePath(route);
 	let unnamedCount = 0;
@@ -89,10 +104,22 @@ export function parse(route) {
 	return tokens;
 }
 
+/**
+ * Converts the given route format string to a regex that can extract param
+ * data from paths matching it.
+ * @return {!RegExp}
+ */
 export function toRegex(route) {
 	return convertTokensToRegex(parse(route));
 }
 
+/**
+ * Extracts data from the given path according to the specified route format.
+ * @param {!Array|string} routeOrTokens Either a route format string or tokens
+ *     previously parsed via the `parse` function.
+ * @param {string} The path to extract param data from.
+ * @return {!Object<string, string>}
+ */
 export function extractData(routeOrTokens, path) {
 	const data = {};
 	const tokens = core.isString(routeOrTokens) ?
