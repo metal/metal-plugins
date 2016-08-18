038a8585efdc1d55f1409f61dffecb4bc9593f05
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2016-08-18 11:46:13 -0700

    Fixes expose multiple files

---
+++ packages/metal-storage/src/all/storage.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import Storage from '../Storage';
+import StorageMechanism from '../mechanism/StorageMechanism';
+import LocalStorageMechanism from '../mechanism/LocalStorageMechanism';
+
+export { Storage, StorageMechanism, LocalStorageMechanism };
