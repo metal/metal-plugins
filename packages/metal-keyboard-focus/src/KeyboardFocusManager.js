425ab734684a788becd880e124fbc9567cc21e59
Maira Bello <maira.araujo@liferay.com>
2016-08-31 15:23:47 -0300

    Adds initial structure

---
+++ packages/metal-keyboard-focus/src/KeyboardFocusManager.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import core from 'metal';
+
+class KeyboardFocusManager {
+}
+
+export default KeyboardFocusManager;
---
+++ packages/metal-keyboard-focus/test/KeyboardFocusManager.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import KeyboardFocusManager from '../src/KeyboardFocusManager';
+
+describe('KeyboardFocusManager', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
