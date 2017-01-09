36fbb41cb6db1cd194d3d2af87c0fb182453cac5
Michael Hadley <michael.hadley@liferay.com>
2017-01-09 12:59:40 -0800

    Source Formatting

--- packages/metal-css-transitions/src/__tests__/TransitionWrapper.js
+++ packages/metal-css-transitions/src/__tests__/TransitionWrapper.js
@@ -14,287 +14,255 @@ describe('TransitionWrapper', function() {
 		}
 	);
 
-	it(
-		'renders',
-		() => {
-			component = new TransitionWrapper();
+	it('renders', () => {
+		component = new TransitionWrapper();
 
-			expect(component);
+		expect(component);
+	});
+
+	it('renders without children', () => {
+		class App extends Component {
+			render() {
+				return (
+					<TransitionWrapper elementClasses="wrapper">
+					</TransitionWrapper>
+				);
+			}
 		}
+
+		component = new App();
+
+		expect(component.element.className).toBe('wrapper');
+	}
 	);
 
-	it(
-		'renders without children',
-		() => {
-			class App extends Component {
-				render() {
-					return (
-						<TransitionWrapper elementClasses="wrapper">
-						</TransitionWrapper>
-						);
+	it('renders with one child', () => {
+		class App extends Component {
+			render() {
+				return (
+					<TransitionWrapper name="test">
+						<div class="child">Child</div>
+					</TransitionWrapper>
+				);
+			}
+		}
+
+		component = new App();
+
+		expect(component.element.querySelector('.child'));
+	});
+
+	it('renders with multiple children', () => {
+		class App extends Component {
+			render() {
+				return (
+					<TransitionWrapper name="test">
+						<div class="child1" key={1}>Child</div>
+						<div class="child2" key={2}>Child</div>
+					</TransitionWrapper>
+				);
+			}
+		}
+
+		component = new App();
+
+		expect(component.element.querySelector('.child1'));
+		expect(component.element.querySelector('.child2'));
+	});
+
+	it('should call setState when new children are supplied', done => {
+		const spy = jest.fn();
+
+		component = new TransitionWrapper();
+
+		component.setState = spy;
+
+		expect(spy).not.toBeCalled();
+		component.props.children = [{}];
+
+		setTimeout(
+			() => {
+				expect(spy).toBeCalled();
+
+				done()
+			},
+			100
+		);
+	});
+
+	it('should call syncChildren with empty args', done => {
+		const spy = jest.fn();
+
+		component = new TransitionWrapper();
+
+		component.setState = spy;
+
+		expect(spy).not.toBeCalled();
+
+		component.props.children = [];
+
+		setTimeout(
+			() => {
+				expect(spy).toBeCalled();
+
+				done()
+			},
+			100
+		);
+	});
+
+	it('should process entering children and call .enter()', () => {
+		component = new TransitionWrapper();
+
+		const spy = jest.fn();
+		const KEY = 1;
+
+		component.components[KEY] = {
+			enter: spy
+		};
+
+		const children = [
+			{
+				props: {
+					key: KEY
 				}
 			}
+		];
 
-			component = new App();
+		expect(spy).not.toBeCalled();
 
-			expect(component.element.className).toBe('wrapper');
-		}
-	);
+		component.handleChildrenEnter_(children, new Map())
 
-	it(
-		'renders with one child',
-		() => {
-			class App extends Component {
-				render() {
-					return (
-						<TransitionWrapper name="test">
-							<div class="child">Child</div>
-						</TransitionWrapper>
-						);
+		expect(spy).toBeCalled();
+
+		delete component.components[KEY];
+	});
+
+	it('should process exiting children and call .leave()', () => {
+		component = new TransitionWrapper();
+
+		const spy = jest.fn();
+		const KEY = 1;
+
+		component.components[KEY] = {
+			leave: spy
+		};
+
+		const children = [
+			{
+				props: {
+					key: KEY
 				}
 			}
+		];
 
-			component = new App();
+		expect(spy).not.toBeCalled();
 
-			expect(component.element.querySelector('.child'));
-		}
-	);
+		component.handleChildrenLeave_(children, new Map());
 
-	it(
-		'renders with multiple children',
-		() => {
-			class App extends Component {
-				render() {
-					return (
-						<TransitionWrapper name="test">
-							<div class="child1" key={1}>Child</div>
-							<div class="child2" key={2}>Child</div>
-						</TransitionWrapper>
-						);
-				}
+		expect(spy).toBeCalled();
+
+		delete component.components[KEY];
+	});
+
+	it('should remove key from childrenMap_ state', () => {
+		component = new TransitionWrapper();
+		const KEY = 1;
+
+		component.state.childrenMap_ = new Map().set(KEY, 'test');
+
+		component.finishLeave_(KEY);
+
+		expect(component.state.childrenMap_.has(KEY)).toBeFalsy();
+	});
+
+	it('should render children in the order they are given when keys are numbers', done => {
+		class App extends Component {
+			render() {
+				return (
+					<TransitionWrapper name="test">
+						{this.props.children}
+					</TransitionWrapper>
+				);
 			}
-
-			component = new App();
-
-			expect(component.element.querySelector('.child1'));
-			expect(component.element.querySelector('.child2'));
 		}
-	);
 
-	it(
-		'should call setState when new children are supplied',
-		(done) => {
-			const spy = jest.fn();
-
-			component = new TransitionWrapper();
-
-			component.setState = spy;
-
-			expect(spy).not.toBeCalled();
-			component.props.children = [{}];
-
-			setTimeout(
-				() => {
-					expect(spy).toBeCalled();
-
-					done()
-				},
-				100
-			);
-		}
-	);
-
-	it(
-		'should call syncChildren with empty args',
-		(done) => {
-			const spy = jest.fn();
-
-			component = new TransitionWrapper();
-
-			component.setState = spy;
-
-			expect(spy).not.toBeCalled();
-
-			component.props.children = [];
-
-			setTimeout(
-				() => {
-					expect(spy).toBeCalled();
-
-					done()
-				},
-				100
-			);
-		}
-	);
-
-	it(
-		'should process entering children and call .enter()',
-		() => {
-			component = new TransitionWrapper();
-
-			const spy = jest.fn();
-			const KEY = 1;
-
-			component.components[KEY] = {
-				enter: spy
-			};
-
-			const children = [
-				{
-					props: {
-						key: KEY
-					}
-				}
-			];
-
-			expect(spy).not.toBeCalled();
-
-			component.handleChildrenEnter_(children, new Map())
-
-			expect(spy).toBeCalled();
-
-			delete component.components[KEY];
-		}
-	);
-
-	it(
-		'should process exiting children and call .leave()',
-		() => {
-			component = new TransitionWrapper();
-
-			const spy = jest.fn();
-			const KEY = 1;
-
-			component.components[KEY] = {
-				leave: spy
-			};
-
-			const children = [
-				{
-					props: {
-						key: KEY
-					}
-				}
-			];
-
-			expect(spy).not.toBeCalled();
-
-			component.handleChildrenLeave_(children, new Map())
-
-			expect(spy).toBeCalled();
-
-			delete component.components[KEY];
-		}
-	);
-
-	it(
-		'should remove key from childrenMap_ state',
-		() => {
-			component = new TransitionWrapper();
-			const KEY = 1;
-
-			component.state.childrenMap_ = new Map().set(KEY, 'test');
-
-			component.finishLeave_(KEY);
-
-			expect(component.state.childrenMap_.has(KEY)).toBeFalsy();
-		}
-	);
-
-	it(
-		'should render children in the order they are given when keys are numbers',
-		(done) => {
-			class App extends Component {
-				render() {
-					return (
-						<TransitionWrapper name="test">
-							{this.props.children}
-						</TransitionWrapper>
-						);
-				}
+		component = new App(
+			{
+				children: [
+					<div class="child" key={1}>1</div>,
+					<div class="child" key={2}>2</div>
+				]
 			}
+		);
 
-			component = new App(
-				{
-					children: [
-						<div class="child" key={1}>1</div>,
-						<div class="child" key={2}>2</div>
-					]
-				}
-			);
+		let children = component.element.querySelectorAll('.child');
 
-			let children = component.element.querySelectorAll('.child');
+		expect(children[0].innerHTML).toBe('1');
+		expect(children[1].innerHTML).toBe('2');
 
-			expect(children[0].innerHTML).toBe('1');
-			expect(children[1].innerHTML).toBe('2');
+		component.props.children = [
+			<div class="child" key={3}>3</div>,
+			<div class="child" key={1}>1</div>,
+			<div class="child" key={2}>2</div>
+		];
 
-			component.props.children = [
-				<div class="child" key={3}>3</div>,
-				<div class="child" key={1}>1</div>,
-				<div class="child" key={2}>2</div>
-			];
+		setTimeout(
+			() => {
+				children = component.element.querySelectorAll('.child');
 
-			setTimeout(
-				() => {
-					children = component.element.querySelectorAll('.child');
+				expect(children[0].innerHTML).toBe('3');
+				expect(children[1].innerHTML).toBe('1');
+				expect(children[2].innerHTML).toBe('2');
 
-					expect(children[0].innerHTML).toBe('3');
-					expect(children[1].innerHTML).toBe('1');
-					expect(children[2].innerHTML).toBe('2');
+				done();
+			},
+			100
+		);
+	});
 
-					done();
-				},
-				100
-			);
-		}
-	);
-
-	it(
-		'should render children in the order they are given when keys are strings',
-		(done) => {
-			class App extends Component {
-				render() {
-					return (
-						<TransitionWrapper name="test">
-							{this.props.children}
-						</TransitionWrapper>
-						);
-				}
+	it('should render children in the order they are given when keys are strings', done => {
+		class App extends Component {
+			render() {
+				return (
+					<TransitionWrapper name="test">
+						{this.props.children}
+					</TransitionWrapper>
+				);
 			}
-
-			component = new App(
-				{
-					children: [
-						<div class="child" key="1">1</div>,
-						<div class="child" key="2">2</div>
-					]
-				}
-			);
-
-			let children = component.element.querySelectorAll('.child');
-
-			expect(children[0].innerHTML).toBe('1');
-			expect(children[1].innerHTML).toBe('2');
-
-			component.props.children = [
-				<div class="child" key="3">3</div>,
-				<div class="child" key="1">1</div>,
-				<div class="child" key="2">2</div>
-			];
-
-			setTimeout(
-				() => {
-					children = component.element.querySelectorAll('.child');
-
-					expect(children[0].innerHTML).toBe('3');
-					expect(children[1].innerHTML).toBe('1');
-					expect(children[2].innerHTML).toBe('2');
-
-					done();
-				},
-				100
-			);
 		}
-	);
+
+		component = new App(
+			{
+				children: [
+					<div class="child" key="1">1</div>,
+					<div class="child" key="2">2</div>
+				]
+			}
+		);
+
+		let children = component.element.querySelectorAll('.child');
+
+		expect(children[0].innerHTML).toBe('1');
+		expect(children[1].innerHTML).toBe('2');
+
+		component.props.children = [
+			<div class="child" key="3">3</div>,
+			<div class="child" key="1">1</div>,
+			<div class="child" key="2">2</div>
+		];
+
+		setTimeout(
+			() => {
+				children = component.element.querySelectorAll('.child');
+
+				expect(children[0].innerHTML).toBe('3');
+				expect(children[1].innerHTML).toBe('1');
+				expect(children[2].innerHTML).toBe('2');
+
+				done();
+			},
+			100
+		);
+	});
 });
