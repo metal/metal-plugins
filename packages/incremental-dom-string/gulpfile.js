28bb10fff52fec2103d4c40a347410d3fe6eeccd
Diego Nascimento <diego.nascimento@liferay.com>
2018-03-28 16:51:39 -0300

    Standardize Karma for testing

--- packages/incremental-dom-string/gulpfile.js
+++ packages/incremental-dom-string/gulpfile.js
@@ -1,5 +1,4 @@
 const babel = require('rollup-plugin-babel');
-const babelRegister = require('babel-register');
 const buffer = require('vinyl-buffer');
 const del = require('del');
 const gulp = require('gulp');
@@ -34,12 +33,6 @@ gulp.task('build:watch', () =>
   gulp.watch('src/*.js', ['build']));
 
 gulp.task('clean', () => del('lib'));
-
-gulp.task('test', () =>
-  gulp.src('test/*.js')
-  .pipe(mocha({
-    compilers: babelRegister({presets: ['env']}),
-  })));
 
 gulp.task('test:watch', () =>
   gulp.watch(['src/*.js', 'test/*.js'], ['test']));
