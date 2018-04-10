2899b067a4943d239167e873c5d46645c4d3b493
Diego Nascimento <diego.nascimento@liferay.com>
2018-04-10 20:06:56 -0300

    Removing unecessary option in gulpfile | Fixes #36

--- packages/metal-storage/gulpfile.js
+++ packages/metal-storage/gulpfile.js
@@ -6,9 +6,5 @@ metal.registerTasks({
 	bundleCssFileName: 'storage.css',
 	bundleFileName: 'storage.js',
 	moduleName: 'metal-storage',
-	noSoy: true,
-	testNodeSrc: [
-		'env/test/node.js',
-		'test/**/*.js'
-	]
+	noSoy: true
 });
