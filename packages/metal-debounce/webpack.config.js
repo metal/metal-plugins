bfdadc78182295ed89945c9bb200c4addd39f8bb
Diego Nascimento <diego.nascimento@liferay.com>
2018-04-11 16:37:21 -0300

    Now build uses webpack instead gulp-metal | Fixes #38

---
+++ packages/metal-debounce/webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const packageName = require('./package.json').name.slice(6);
+
+let common = {
+	entry: `./src/${packageName}.js`,
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /(node_modules)/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					compact: false,
+					presets: ['env'],
+					plugins: ['babel-plugin-transform-node-env-inline'],
+				},
+			},
+		}],
+	},
+};
+
+let bundle = Object.assign({
+	devtool: 'source-map',
+	output: {
+		library: 'metal',
+		libraryTarget: 'global',
+		filename: `./build/globals/${packageName}.js`,
+	},
+	plugins: [
+		new webpack.optimize.ModuleConcatenationPlugin(),
+	],
+}, common);
+
+let minified = Object.assign({
+	output: {
+		library: 'metal',
+		libraryTarget: 'global',
+		filename: './build/globals/debounce-min.js',
+	},
+	plugins: [
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new UglifyJSPlugin(),
+	],
+}, common);
+
+
+module.exports = [bundle, minified];
