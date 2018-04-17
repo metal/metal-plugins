9f80aef0c783ab0da3ab5d37e37ae0c236f513d8
Diego Nascimento <diego.nascimento@liferay.com>
2018-04-17 15:21:45 -0300

    Adds JSDOC for an implicit import in debounce.js | Fixes #38

--- packages/metal-debounce/src/debounce.js
+++ packages/metal-debounce/src/debounce.js
@@ -1,7 +1,8 @@
 'use strict';
 
-/* eslint-disable-next-line */
-import from from 'core-js/es6/array';
+// This importation is used to polyfill Array.from method that is used in
+// _toConsumableArray() function in babel glue to this rest-spread utilization
+import from from 'core-js/es6/array'; //eslint-disable-line
 
 /**
  * Debounces function execution.
