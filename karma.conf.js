c4844b793290a7d69a896172caeb0ae083b27241
Diego Nascimento <diego.nascimento@liferay.com>
2018-04-18 19:23:49 -0300

    Now we are using headless chrome in karma and using latest webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,40 @@
 'use strict';
 
 const lernaJson = require('./lerna.json');
+const webpack = require('webpack');
+let performance = String(process.env.PERFORMANCE) !== 'false';
+
+let localLaunchers = {
+    ChromeNoSandboxHeadless: {
+        base: 'Chrome',
+        flags: [
+            '--no-sandbox',
+            '--headless',
+            '--disable-gpu',
+            // Without a remote debugging port, Google Chrome exits immediately.
+            '--remote-debugging-port=9333'
+        ]
+    }
+};
 
 module.exports = function (config) {
     config.set({
-        browsers: ['Chrome'],
+        browsers: Object.keys(localLaunchers),
 
-        customHeaders: [
-			{
-				match: './packages/metal-ajax/test/data/data.json',
-				name: 'Content-Length',
-				value: 315186
-			}
-		],
+        customLaunchers: localLaunchers,
 
-        frameworks: ['mocha', 'chai', 'sinon'],
+        customHeaders: [{
+            match: './packages/metal-ajax/test/data/data.json',
+            name: 'Content-Length',
+            value: 315186
+        }],
+
+        frameworks: ['mocha', 'chai-sinon'],
 
         plugins: [
-            'karma-chai',
+            'karma-chai-sinon',
             'karma-chrome-launcher',
             'karma-mocha',
-            'karma-sinon',
             'karma-webpack'
         ],
 
@@ -29,6 +43,8 @@ module.exports = function (config) {
         },
 
         webpack: {
+            mode: 'development',
+
             module: {
                 rules: [{
                     test: /\.js$/,
@@ -36,16 +52,38 @@ module.exports = function (config) {
                     use: {
                         loader: 'babel-loader',
                         options: {
-                            compact: false,
+                            compact: true,
+                            comments: false,
                             presets: ['env']
                         }
                     }
                 }]
+            },
+
+            plugins: [
+                new webpack.DefinePlugin({
+                    DISABLE_FLAKEY: !!String(process.env.FLAKEY).match(/^(0|false)$/gi),
+                    ENABLE_PERFORMANCE: performance
+                }),
+            ],
+
+            performance: {
+                hints: false
             }
+        },
+
+        webpackMiddleware: {
+            logLevel: 'error',
+            stats: 'errors-only'
         },
 
         singleRun: true,
 
+		browserLogOptions: { terminal: true },
+		browserConsoleLogOptions: { terminal: true },
+
+        browserNoActivityTimeout: 5 * 60 * 1000,
+        
         client: {
             mocha: {
                 timeout: 4000,
@@ -65,11 +103,11 @@ module.exports = function (config) {
             },
 
             {
-				pattern: 'packages/metal-ajax/test/data/data.json',
-				watched: false,
-				included: false,
-				served: true
-			}
+                pattern: 'packages/metal-ajax/test/data/data.json',
+                watched: false,
+                included: false,
+                served: true
+            }
         ],
 
         singleRun: true,
