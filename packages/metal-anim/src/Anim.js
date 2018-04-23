9e2507cea6ef3ec24a9c321e11e83ddd51ff0116
Diego Nascimento <diego.nascimento@liferay.com>
2018-04-23 16:31:51 -0300

    Fixes lint errors

--- packages/metal-anim/src/Anim.js
+++ packages/metal-anim/src/Anim.js
@@ -1,73 +1,79 @@
 'use strict';
 
 import core from 'metal';
-import { dom, features } from 'metal-dom';
+import {dom, features} from 'metal-dom';
 
+/**
+ * Anim class.
+ */
 class Anim {
 	/**
 	 * Emulates animation or transition end event, the end event with longer
 	 * duration will be used by the emulation. If they have the same value,
 	 * transitionend will be emulated.
 	 * @param {!Element} element
-	 * @param {number=} opt_durationMs
+	 * @param {number=} optDurationMs
 	 * @return {!Object} Object containing `abort` function.
 	 */
-	static emulateEnd(element, opt_durationMs) {
-		if (this.getComputedDurationMs(element, 'animation') > this.getComputedDurationMs(element, 'transition')) {
-			return this.emulateEnd_(element, 'animation', opt_durationMs);
+	static emulateEnd(element, optDurationMs) {
+		if (
+			this.getComputedDurationMs(element, 'animation') >
+			this.getComputedDurationMs(element, 'transition')
+		) {
+			return this.emulateEnd_(element, 'animation', optDurationMs);
 		} else {
-			return this.emulateEnd_(element, 'transition', opt_durationMs);
+			return this.emulateEnd_(element, 'transition', optDurationMs);
 		}
 	}
 
 	/**
-	 * Emulates animation end event. If `opt_durationMs` not specified the value
+	 * Emulates animation end event. If `optDurationMs` not specified the value
 	 * will read from computed style for animation-duration.
 	 * @param {!Element} element
-	 * @param {number=} opt_durationMs
+	 * @param {number=} optDurationMs
 	 * @return {!Object} Object containing `abort` function.
 	 */
-	static emulateAnimationEnd(element, opt_durationMs) {
-		return this.emulateEnd_(element, 'animation', opt_durationMs);
+	static emulateAnimationEnd(element, optDurationMs) {
+		return this.emulateEnd_(element, 'animation', optDurationMs);
 	}
 
 	/**
-	 * Emulates transition end event. If `opt_durationMs` not specified the
+	 * Emulates transition end event. If `optDurationMs` not specified the
 	 * value will read from computed style for transition-duration.
 	 * @param {!Element} element
-	 * @param {number=} opt_durationMs
+	 * @param {number=} optDurationMs
 	 * @return {!Object} Object containing `abort` function.
 	 */
-	static emulateTransitionEnd(element, opt_durationMs) {
-		this.emulateEnd_(element, 'transition', opt_durationMs);
+	static emulateTransitionEnd(element, optDurationMs) {
+		this.emulateEnd_(element, 'transition', optDurationMs);
 	}
 
 	/**
 	 * Emulates transition or animation end.
 	 * @param {!Element} element
 	 * @param {string} type
-	 * @param {number=} opt_durationMs
+	 * @param {number=} optDurationMs
 	 * @return {!Object} Object containing `abort` function.
 	 * @protected
 	 */
-	static emulateEnd_(element, type, opt_durationMs) {
-		var duration = opt_durationMs;
-		if (!core.isDef(opt_durationMs)) {
+	static emulateEnd_(element, type, optDurationMs) {
+		let duration = optDurationMs;
+		if (!core.isDef(optDurationMs)) {
 			duration = this.getComputedDurationMs(element, type);
 		}
 
-		var delayed = setTimeout(function() {
+		let delayed = setTimeout(function() {
 			dom.triggerEvent(element, features.checkAnimationEventName()[type]);
 		}, duration);
 
-		var abort = function() {
+		let abort = function() {
 			clearTimeout(delayed);
 			hoistedEvtHandler.removeListener();
 		};
-		var hoistedEvtHandler = dom.once(element, type + 'end', abort);
+		let hoistedEvtHandler = dom.once(element, type + 'end', abort);
 
 		return {
-			abort: abort
+			abort: abort,
 		};
 	}
 
@@ -78,7 +84,13 @@ class Anim {
 	 * @return {number} The computed duration in milliseconds.
 	 */
 	static getComputedDurationMs(element, type) {
-		return (parseFloat(window.getComputedStyle(element, null).getPropertyValue(type + '-duration')) || 0) * 1000;
+		return (
+			(parseFloat(
+				window
+					.getComputedStyle(element, null)
+					.getPropertyValue(type + '-duration')
+			) || 0) * 1000
+		);
 	}
 }
 
