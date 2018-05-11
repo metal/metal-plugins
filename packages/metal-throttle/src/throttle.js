d4847bd07be83ddcf2eb42309f24890a87f883b9
Pablo Molina <contact@pablomolina.me>
2018-05-11 11:58:59 +0200

    Add metal-throttle 1.0.0

---
+++ packages/metal-throttle/src/throttle.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// This dependency is used to polyfill Array.from method that is used in
+// _toConsumableArray() function in babel glue to this rest-spread utilization
+import from from 'core-js/es6/array'; //eslint-disable-line
+
+/**
+ * Debounces function execution.
+ * @param {!function()} fn
+ * @param {number} [delay=0]
+ * @return {!function()}
+ */
+function throttle(fn, delay = 0) {
+	const context = this;
+	let last = 0;
+
+	return (...args) => {
+		const next = last + delay;
+		const now = Date.now();
+
+		if (next < now) {
+			try {
+				fn.call(context, ...args);
+			} catch (error) {
+				throw error;
+			} finally {
+				last = Date.now();
+			}
+		}
+	};
+}
+
+export default throttle;
+export {throttle};
