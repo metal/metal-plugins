4b7fa87bcbfb43451e4538a1485ad8cfaa2605bb
Diego Nascimento <diego.nascimento@liferay.com>
2018-09-10 15:01:49 -0300

    When finish a drag, original event is propagated listening 'end' event | Fixes #82

--- packages/metal-drag-drop/src/Drag.js
+++ packages/metal-drag-drop/src/Drag.js
@@ -421,7 +421,7 @@ class Drag extends State {
 	 * Triggered when the mouse drag action ends.
 	 * @protected
 	 */
-	handleDragEndEvent_() {
+	handleDragEndEvent_(event) {
 		if (this.autoScroll) {
 			this.autoScroll.stop();
 		}
@@ -511,7 +511,7 @@ class Drag extends State {
 	 */
 	handleKeyDown_(event) {
 		if (event.keyCode === 27 && this.isDragging()) {
-			this.handleDragEndEvent_();
+			this.handleDragEndEvent_(event);
 		}
 	}
 
@@ -568,7 +568,7 @@ class Drag extends State {
 				event.keyCode === 27
 			) {
 				// Enter, space or esc during drag will end it.
-				this.handleDragEndEvent_();
+				this.handleDragEndEvent_(event);
 			}
 		} else if (event.keyCode === 13 || event.keyCode === 32) {
 			// Enter or space will start the drag action.
